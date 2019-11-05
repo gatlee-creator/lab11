315c1130c54ef593da6619c9f7c1916f35acf21b
gatlee-creator <gatlee@highpoint.edu>
2019-11-05 14:13:38 -0500

    started testing stringList

--- templateTestProg.cpp
+++ templateTestProg.cpp
@@ -1,7 +1,7 @@
 //Data: 2.85 6.74 28.93 61.22 10.00 45.34 61.78 5.75
 
 #include <iostream>
- 
+#include <string>
 #include "unorderedArrayListType.h"
 
 using namespace std; 
@@ -107,54 +107,73 @@ Testing Integer list & and all previous stub functions
 in this section
 =====================================
 */
-    cout << "Creating integerList of size 5" << endl;
-    unorderedArrayListType<int> integerList(5);
+    // cout << "Creating integerList of size 5" << endl;
+    // unorderedArrayListType<int> integerList(5);
     
-    //here we will test isEmpty()
-    cout << "Testing if integerList is empty. Should return 1" << endl;
-    cout << integerList.isEmpty() << endl;
+    // //here we will test isEmpty()
+    // cout << "Testing if integerList is empty. Should return 1" << endl;
+    // cout << integerList.isEmpty() << endl;
 
-    //test isFull()
-    cout << "Testing if integerList isFull. Should return 0" << endl;
-    cout << integerList.isFull() << endl;
+    // //test isFull()
+    // cout << "Testing if integerList isFull. Should return 0" << endl;
+    // cout << integerList.isFull() << endl;
 
-    //call insertAt 2 times 
-    cout << "Inserting 2 and 5 at positions 0, 1" << endl;
-    integerList.insertAt(0, 2);
-    integerList.insertAt(1, 5);
-    cout << "integerList: ";
-    integerList.print();
+    // //call insertAt 2 times 
+    // cout << "Inserting 2 and 5 at positions 0, 1" << endl;
+    // integerList.insertAt(0, 2);
+    // integerList.insertAt(1, 5);
+    // cout << "integerList: ";
+    // integerList.print();
 
-    //check listSize()
-    cout << "Calling integerList listSize. Should return 2" << endl;
-    cout << integerList.listSize() << endl;
+    // //check listSize()
+    // cout << "Calling integerList listSize. Should return 2" << endl;
+    // cout << integerList.listSize() << endl;
 
-    //call maxListSize()
-    cout << "Calling maxListSize. Should return 5" << endl;
-    cout << integerList.maxListSize() << endl;
+    // //call maxListSize()
+    // cout << "Calling maxListSize. Should return 5" << endl;
+    // cout << integerList.maxListSize() << endl;
 
-    //test clearList() here
-    cout << "Calling clearList and checking the new length" << endl;
-    integerList.clearList();
-    cout << "integerList: ";
-    integerList.print();
-    cout << "length: " << integerList.listSize() << endl;
+    // //test clearList() here
+    // cout << "Calling clearList and checking the new length" << endl;
+    // integerList.clearList();
+    // cout << "integerList: ";
+    // integerList.print();
+    // cout << "length: " << integerList.listSize() << endl;
 
-    //try the copy constructor 
-    cout << "Creating newIntegerList through copy constructor" << endl;
-    unorderedArrayListType <int>newIntegerList(integerList);
-    cout << "newIntegerList (should be empty): ";
-    newIntegerList.print();
+    // //try the copy constructor 
+    // cout << "Creating newIntegerList through copy constructor" << endl;
+    // unorderedArrayListType <int>newIntegerList(integerList);
+    // cout << "newIntegerList (should be empty): ";
+    // newIntegerList.print();
 
-    //test for shallow copy by calling insertAt()
-    cout << "Testing for shallow copy by inputting data into newIntegerList" <<endl;
-    newIntegerList.insertAt(0,3);
-    newIntegerList.insertAt(1,4);
-    newIntegerList.insertAt(2,2);
-    cout << "newIntegerList: ";
-    newIntegerList.print();
-    cout << "integerList (should be empty): ";
-    integerList.print(); 
+    // //test for shallow copy by calling insertAt()
+    // cout << "Testing for shallow copy by inputting data into newIntegerList" <<endl;
+    // newIntegerList.insertAt(0,3);
+    // newIntegerList.insertAt(1,4);
+    // newIntegerList.insertAt(2,2);
+    // cout << "newIntegerList: ";
+    // newIntegerList.print();
+    // cout << "integerList (should be empty): ";
+    // integerList.print(); 
+
+/*
+=====================================
+Testing String List 
+=====================================
+*/
+
+    cout << "Creating stringList with default constructor" << endl;
+    unorderedArrayListType <string>stringList;
+
+    //call maxSize to ensure default constructor worked 
+    cout << "Printing maxSize of stringList. Should be 100" << endl;
+    cout << stringList.maxListSize() << endl;
+
+    //test insertEnd in a loop here 
+    cout << "Inserting \"Apple\" into stringList 50 times" << endl;
+    for(int i=0; i < 50; i++)
+        stringList.insertEnd("Apple");
+    stringList.print();
 
 
     return 0;                                   
