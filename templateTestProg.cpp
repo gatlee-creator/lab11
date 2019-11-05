7f5595bb387361b529fa4793617fbe735fc6b0db
gatlee-creator <gatlee@highpoint.edu>
2019-11-05 10:47:17 -0500

    All non-stub functions tested in main

--- templateTestProg.cpp
+++ templateTestProg.cpp
@@ -10,7 +10,8 @@ int main()
 {
     unorderedArrayListType<double> numList(25); 
 
-    double num;                                
+    double num;
+    int loc;                                
 
     cout << "Enter 8 numbers: ";        
 
@@ -46,6 +47,65 @@ int main()
         cout <<  num << " found in numList." << endl;   
     else                                        
         cout <<  num << " is not in numList." << endl;  
+
+/*
+==============================================================
+NEW TEST CODE EXTENSION BELOW (11 / 5 / 19)
+==============================================================
+*/
+
+    // //lets test insertAt here -STUB 
+    // cout << "Insert item at a location (loc, num): " << endl;
+    // cin >> loc;
+    // cin >> num; 
+    // numList.insertAt(loc, num);
+    // numList.print();
+
+    //lets test replaceAt here 
+    cout << "Replace item at a location: " << endl;
+    cout << "Type location: ";
+    cin >> loc;
+    cout << "Type replacement number: ";
+    cin >> num;
+    numList.replaceAt(loc, num);
+    cout << "Updated list: ";
+    numList.print();
+
+    //test isItemAtEqual here - SOMETHNG BROKEN HERE?
+    cout << "Check if item is equal at a location" << endl;
+    cout << "Type location: ";
+    cin >> loc;
+    cout << "Type comparison number: ";
+    cin >> num;
+    bool result = numList.isItemAtEqual(loc, num);
+    if(result == 0)
+        cout << "Item is equal" << endl;
+    else
+        cout << "Item is NOT equal" << endl;
+
+    //test removeAt here
+    cout << "Remove an item at a location" << endl;
+    cout << "Type location: ";
+    cin >> loc;
+    numList.removeAt(loc);
+    cout << "Updated list: ";
+    numList.print();
+
+    //will test retrieveAt here 
+    cout << "Retrieve a number at a location" << endl;
+    cout << "Type location: ";
+    cin >> loc;
+    cout << "Type retrievable number: ";
+    cin >> num;
+    numList.retrieveAt(loc, num);
+    cout << "Retrieved number: " << num << endl;
+
+    //testing overloaded assignment operator here
+    cout << "Assigning a newList to numList" << endl;
+    unorderedArrayListType<double> newList(5);
+    newList = numList;
+    cout << "newList: ";
+    newList.print(); 
 
     return 0;                                   
 }
